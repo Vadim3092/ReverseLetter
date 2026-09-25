8f7ae290ce4de822efd62b9855db78d0f0a9e6a8
Vadim Grishin <Greeshin@bk.ru>
2026-09-25 11:54:28 +0300

    Добавь решение ReverseLetter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        String s = "J@va the be$t!123";
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            } else if (!Character.isLetter(chars[right])) {
+                right--;
+            } else {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        System.out.print(new String(chars));
+
     }
 }
